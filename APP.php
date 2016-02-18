dd2259006a9790726d29648dda577d39005f4eca
mertyarba <mertyarba@greeny.cs.tlu.ee>
2016-02-18 12:30:37 +0200

    added warnings and fixed some lines

--- APP.php
+++ APP.php
@@ -4,7 +4,7 @@
 	if (isset ($_GET["message"])){
 		
 		//only if there is message in the URL
-		echo "there is message";
+		//echo "there is message";
 		
 		// if it is empty
 		if (empty ($_GET["message"])){
@@ -18,16 +18,16 @@
 		//echo "There is no such thing as message";
 	}
 	
-	}
+	
 	
 	
 	
 	//Getting the message from the address
 	//if there is $name= .. then $_GET ["name"]
-	$my_message = $_GET ["message"];
-	$to = $_GET ["to"];
-	$urgency = $_GET ["urgency"];
-	echo "My message is " .$my_message. " and it is to " .$to;
+	//$my_message = $_GET ["message"];
+	//$to = $_GET ["to"];
+	//$urgency = $_GET ["urgency"];
+	//echo "My message is " .$my_message. " and it is to " .$to;
 	
 	
 ?>
