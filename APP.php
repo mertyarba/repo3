8a42838614ff8ae8918c411f04accee051e9ceab
mertyarba <mertyarba@greeny.cs.tlu.ee>
2016-02-18 11:32:57 +0200

    added urgency section

--- APP.php
+++ APP.php
@@ -5,7 +5,8 @@
 	//if there is $name= .. then $_GET ["name"]
 	$my_message = $_GET ["message"];
 	$to = $_GET ["to"];
-	echo "My message is " .$my_message. " and it is to ".$to;
+	$urgency = $_GET ["urgency"];
+	echo "My message is " .$my_message. " and it is to ".$to. "IT IS URGENT!!!".$urgency;
 	
 	
 ?>
@@ -17,6 +18,8 @@
 	<input type="text" name="to"><br>
 	<label for="message">Message* <label><br>
 	<input type="text" name="message"><br>
+	<label for="urgency">Urgency* <label><br>
+	<input type="text" name="Urgency"><br>
 	<input type="submit" value="save to DB">
 	
 	
