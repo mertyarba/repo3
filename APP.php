5a80a1e507fb13b223ddc843437617cc897c0092
mertyarba <mertyarba@greeny.cs.tlu.ee>
2016-02-18 10:54:01 +0200

    added bunch of codes

--- APP.php
+++ APP.php
@@ -1,1 +1,19 @@
-APP
+<?php
+
+
+
+
+
+?>
+
+<h2> First Application </h2>
+
+<form method="get">
+	<label for="message">Message* <label><br>
+	<input type="text" name="message"><br>
+	<input type="submit" value="save to DB">
+	
+
+
+
+<form>
