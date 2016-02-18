c4627d887fb283926562f7149745b674e2db5c5a
mertyarba <mertyarba@greeny.cs.tlu.ee>
2016-02-18 11:24:21 +0200

    fixed some lines

--- APP.php
+++ APP.php
@@ -5,17 +5,17 @@
 	//if there is $name= .. then $_GET ["name"]
 	$my_message = $_GET ["message"];
 	echo "the message " .$my_message;
-	$to = $_GET ["message"];
-	echo "is from " .$to;
+	$to = $_GET ["to"];
+	echo " is from " .$to;
 ?>
 
 <h2> First Application </h2>
 
 <form method="get">
+	<label for="to">To* <label><br>
+	<input type="text" name="to"><br>
 	<label for="message">Message* <label><br>
 	<input type="text" name="message"><br>
-	<label for="to">To* <label><br>
-	<input type="text" name="is for"><br>
 	<input type="submit" value="save to DB">
 	
 	
