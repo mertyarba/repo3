e05caf2c125f937f5ebbd454a3c3523c4070773b
mertyarba <mertyarba@greeny.cs.tlu.ee>
2016-02-18 11:15:20 +0200

    changed the places of two lines

--- APP.php
+++ APP.php
@@ -14,9 +14,10 @@
 <form method="get">
 	<label for="message">Message* <label><br>
 	<input type="text" name="message"><br>
-	<input type="submit" value="save to DB">
 	<label for="to">To* <label><br>
 	<input type="text" name="is for"><br>
+	<input type="submit" value="save to DB">
+	
 	
 	
 
